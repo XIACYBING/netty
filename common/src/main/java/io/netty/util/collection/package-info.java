0f47ed4f657381535141780f8328bd9e7c509d94
wang.yubin <359474377@qq.com>
2023-04-27 16:18:00 +0800

    生成项目运行缺失的collection包，并添加相关说明

---
+++ common/src/main/java/io/netty/util/collection/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * collection包：通过执行{@code mvn clean install -pl common -DskipTests=true -Dcheckstyle.skip=true}命令，通过{@see common/src
+ * /main/templates}文件夹下的相关文件生成，其中应该还有用到{@see common/src/main/script}文件夹下的脚本
+ *
+ * @author wang.yubin
+ * @since 2023/4/27
+ */
+package io.netty.util.collection;
